db027b918fe05e975894d0576f38556dbf0d7f04
Swapnil Sagar <CSCSPV1389@csccloud.in>
2023-04-19 15:30:10 +0530

    First Commit

---
+++ secret_service/agents/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Agent
+
+# Register your models here.
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "code_name", "location", "secret_mission")
+
+admin.site.register(Agent, AgentAdmin)
